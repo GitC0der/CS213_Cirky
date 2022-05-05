2884e0bfba1d2f5a5a24c7f13f63eb98c7cdaea3
“Danny <danny.seel@epfl.ch>
2022-05-05 11:01:53 +0200

    Removed some SGS items

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -7,10 +7,8 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
 
-    public Text scoreTextPlayer1;
-    public Text scoreTextPlayer2;
-    int scorePlayer1 = 0;
-    int scorePlayer2 = 0;
+    public Text scoreTextPlayer;
+    int scorePlayer = 0;
 
     private void Awake() {
         instance = this;
@@ -19,30 +17,19 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scoreTextPlayer1.text = "P1: " + scorePlayer1.ToString() + " Points";
-        scoreTextPlayer2.text = "P2: " + scorePlayer2.ToString() + " Points";
+        scoreTextPlayer.text = "P1: " + scorePlayer.ToString() + " Points";
     }
 
     // Update is called once per frame
     public void AddPoint(string name)
     {
-        if (name.Equals("Player CelluloAgent_1")) {
-            scorePlayer1 += 1;
-            scoreTextPlayer1.text = "P1: " + scorePlayer1.ToString() + " Points";
-        } else /*if (name.Equals("Player CelluloAgent_2"))*/ {
-            scorePlayer2 += 1;
-            scoreTextPlayer2.text = "P2: " + scorePlayer2.ToString() + " Points";
-        }
+        scorePlayer += 1;
+        scoreTextPlayer.text = scorePlayer.ToString() + " Points";  
     }
 
     public void RemovePoint(string name)
     {
-        if (name.Equals("Player CelluloAgent_1")) {
-            scorePlayer1 -= 1;
-            scoreTextPlayer1.text = "P1: " + scorePlayer1.ToString() + " Points";
-        } else /*if (name.Equals("Player CelluloAgent_2"))*/ {
-            scorePlayer2 -= 1;
-            scoreTextPlayer2.text = "P2: " + scorePlayer2.ToString() + " Points";
-        }
+        scorePlayer -= 1;
+        scoreTextPlayer.text = scorePlayer.ToString() + " Points";
     }
 }
